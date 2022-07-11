ea0ef53ae3682e63375e367693fe45611220f409
mjekott <ekottmfon@yahoo.com>
2022-07-11 17:56:13 +0100

    ci: removed webpack config in preview.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,26 +21,5 @@ module.exports = {
   ],
   core: {
     builder: 'webpack5'
-  },
-  webpackFinal: config => {
-    /**
-     * Add support for alias-imports
-     * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
-     */
-    config.resolve.alias = {
-      ...config.resolve?.alias,
-      '@': [path.resolve(__dirname, '../')]
-    };
-
-    /**
-     * Fixes font import with /
-     * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
-     */
-    config.resolve.roots = [
-      path.resolve(__dirname, '../public'),
-      'node_modules'
-    ];
-
-    return config;
   }
 };
