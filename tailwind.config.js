f5920a44dad3dfe5c1ca2f2085b35cc04d29978e
mjekott <ekottmfon@yahoo.com>
2022-07-11 17:51:56 +0100

    ci: addeded screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1440px'
+    },
     extend: {}
   },
   plugins: []
